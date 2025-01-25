5a4c89ef287754be7aea4fe84707de2f820db82c
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-01-25 20:11:16 -0300

    fix: Styles

--- src/components/movie-card/movie-card.component.tsx
+++ src/components/movie-card/movie-card.component.tsx
@@ -1,18 +1,34 @@
 import { Card } from "../../interfaces/card.interface";
+import { StartRating } from "../start-rating/start-rating.component";
 import './movie-card.component.scss';
+
+const IMG_URL = 'https://image.tmdb.org/t/p/original';
 
 export const MovieCard = (props: Card) => { 
     const movie = props.movie;
     
     return (
         <li className="movie-card">
-            <h5 className="title">{movie.title}</h5>
-            <p className="description">{movie.overview}</p>
-            <img 
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} 
-                alt={movie.title} 
-            />
-            <p>{movie.vote_average}</p>
+            <div className="movie-poster">
+                <img 
+                    src={`${IMG_URL}${movie.poster_path}`} 
+                    alt={movie.title} 
+                />
+            </div>
+            <div className="movie-info">
+                <h5 className="movie-title">{movie.title}</h5>
+                { movie.vote_average > 0 && <StartRating rating={movie.vote_average} />}
+                <div className="hidden-content">
+                    { movie.overview &&
+                        <p className="movie-description">{
+                            movie.overview.length > 100
+                                ? `${movie.overview.substring(0, 100)}...`
+                                : movie.overview
+                        }</p>                     
+                    }
+                    <button className="btn-default">Ver mas</button>
+                </div>
+            </div>
         </li>
     );   
 };
--- src/components/movie-list/movie-list.component.tsx
+++ src/components/movie-list/movie-list.component.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Movie } from '../../interfaces/movie.interface';
 import { MovieCard } from '../movie-card/movie-card.component';
+import ReactLoading from 'react-loading';
 import './movie-list.component.scss';
 
 const API_KEY = '3c27cf2da95a9372232a01814c2dc0ee';
@@ -8,11 +9,14 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie';
 
 export const MovieList = () => {
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
     const getMovies = () => {
-        fetch(`${API_URL}?api_key=${API_KEY}`)
+        fetch(`${API_URL}?api_key=${API_KEY}&language=es-ES`)
             .then((res) => res.json())
             .then((data) => {
-                setMovies(data.results);                
+                setMovies(data.results);  
+                setLoading(false);              
             })
             .catch((error) => console.error(error));
     };
@@ -22,12 +26,15 @@ export const MovieList = () => {
     }, []);    
 
     return (
-        <ul className="movie-list">
-            { 
-                movies.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie}/>
-                ))
-            }
-        </ul>
+        <>  
+            { loading 
+                ?   <div className='loading-container'><ReactLoading type="spin" color='#f1c40f' height={'5%'} width={'5%'}/></div>
+                :   <ul className="movie-list">{ 
+                        movies.map((movie) => (
+                            <MovieCard key={movie.id} movie={movie}/>
+                        ))
+                    }</ul>
+            }            
+        </>
     );
 };
