c40273c0c3bcd69d6d5b12756085a96590a69f02
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-01-26 11:29:46 -0300

    fix: App design

--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -3,7 +3,10 @@ import './nav-bar.component.scss';
 export const NavBar = () => {
   return (
     <nav className="navbar">
-      <h1 className="title">Movie App</h1>
+      <div className="title">
+        MovieApp
+        <span className="subtitle">Lo mejor del cine en un solo lugar</span>
+      </div>
     </nav>
   )
 };
