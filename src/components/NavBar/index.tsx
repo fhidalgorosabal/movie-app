1c1ccd156117deb0b5c72ed75419b67ed8d12c28
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-01-28 23:44:24 -0300

    feature: Create menu in navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 import './styles.scss';
 
 export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="title">
-        MovieApp
-        <span className="subtitle">Lo mejor del cine en un solo lugar</span>
+      <div className="navbar-content">
+        <div className="title">
+          MovieApp
+        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <FaBars />
+        </button>
+        <ul className={`menu ${isMenuOpen ? 'open' : ''}`}>
+          <li className="menu-item">Películas</li>
+          <li className="menu-item">Series-TV</li>
+          <li className="menu-item">People</li>
+        </ul>
       </div>
     </nav>
-  )
+  );
 };
